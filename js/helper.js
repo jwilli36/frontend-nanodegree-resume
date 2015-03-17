8c41796ebb7b6cd9228c9150084539463a5c2b68
Joffrey Williams <xxxxx@xxxxx.com>
2015-03-16 21:08:01 -0600

    Update Resume files

--- js/helper.js
+++ js/helper.js
@@ -12,26 +12,26 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderName = '<h1 id="name">Joffrey Williams</h1>';
+var HTMLheaderRole = '<span>Front End Web Developer</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="biopic tilt pic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<a href="#">%data%</a>';
+var HTMLworkTitle = '<a href="#">%data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
@@ -40,18 +40,18 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" class="projectimg  tilt  pic">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolName = '<a href="#">%data%</a>'
+var HTMLschoolDegree = '<a href="#">%data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineSchool = '<a - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
@@ -170,7 +170,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infowindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +233,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+  window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+  window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+  });
---
+++ js/resumeBuilder.js
@@ -0,0 +1,225 @@
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName); 
+
+var bio ={
+	"name" : " Joffrey Williams",
+	"role": " Web Developer ",
+	"contacts": {
+	  "mobile": "773-415-3834",
+	  "email": " joffrey.williams1@gmail.com ",
+	  "github" : " jwilli36 ",
+	  "linkedin": "www.linkedin.com/in/jjwwwil",
+	  "location": "Chicago"
+	},
+	"welcomeMessage": "Welcome to my Professional resume page",  
+	"skills" : [
+	 "Six Sigma Greenbelt", "Process Improvement", "Quality Management", "Training and Development", "Labor Relations"],
+	"bioPic": "\images/IMG_24.png"
+}
+   
+	
+    var formattedmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").prepend(formattedmobile);
+	$("#footerContacts").append(formattedmobile);
+	
+	var formattedemail = HTMLemail.replace("%data%", bio.contacts.email); 
+	$("#topContacts").prepend(formattedemail);
+	$("#footerContacts").append(formattedemail);
+	
+	var formattedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts").prepend(formattedgithub);
+	$("#footerContacts").append(formattedgithub);
+	
+	var formattedlinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
+	$("#topContacts").append(formattedlinkedin);
+	$("#footerContacts").append(formattedlinkedin);
+	
+	var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts").append(formattedlocation);
+	$("#footerContacts").append(formattedlocation);
+	
+	var formattedpic = HTMLbioPic.replace("%data%", bio.bioPic);
+	$("#topContacts").prepend(formattedpic);
+	
+	var formattedMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage); 
+	$("#topContacts").append(formattedMsg);
+	
+    $("#header").append(HTMLskillsStart);
+	
+  for (var i = 0; i < bio.skills.length; i++){ 
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+  $("#skills").append(formattedSkill);
+  }
+var education = { 
+  "schools":[
+     {
+	   "name": "DeVry University",
+	   "location": "Chicago, IL, US",
+	   "degree": "BS",
+	   "major" : ["Telecommunications Management", " Finance"],
+	   "dates" : 2003,
+	   "url": "www.Devry.edu"
+	 },
+	 {
+	   "name": "DePaul University",
+	   "location": "Chicago, IL, US",
+	   "degree": "Masters of Science",
+	   "major" : "Telecommunication Systems",
+	   "dates" : 2005,
+	   "url": "www.depaul.edu"
+	 }
+	],
+  "onlineCourses":[
+     {
+	   "title": "Front End Web Development",
+	   "school" : "Udacity",
+	   "dates": 2014,
+	   "url": "http://www.udacity.com/"
+	 },
+	{
+	   "title": "VOIP",
+	   "school" : "DePaul University",
+	   "dates": 2004,
+	   "url": "http://www.depaul.edu"
+	 } 
+	]
+}
+function displayEducation() {
+for (school in education.schools) {
+  $("#education").append(HTMLschoolStart);
+  var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+  var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+  var formattedNameDegree = formattedschoolName + formattedschoolDegree;
+  var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+  $(".education-entry:last").append(formattedschoolLocation);
+  $(".education-entry:last").append(formattedNameDegree);
+  
+  
+  var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+  $(".education-entry:last").append(formattedschoolDates);
+  
+  
+  var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+  $(".education-entry:last").append(formattedschoolMajor);
+	}
+	
+	$("#education").append(HTMLonlineClasses);
+ for (onlineCourse in education.onlineCourses) {
+    $("#education").append(HTMLschoolStart);
+  var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title);
+  var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school);
+  var formattedTitleSchool = formattedonlineTitle + formattedonlineSchool;
+  
+  $(".education-entry:last").append(formattedTitleSchool);
+  
+  
+  var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates);
+  $(".education-entry:last").append(formattedonlineDates);
+  
+  
+  var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url);
+  $(".education-entry:last").append(formattedonlineURL);
+	}
+}
+displayEducation();
+var work = {
+  "jobs": [
+     { 
+	    "employer" : " ATT ",
+	    "title" : "Manager Business Services",
+		"location": "Chicago, Il",
+		"dates": "Nov 1997 - Current",
+	    "description": "Provide leadership and mentoring to a team of 13 Customer Service Field Technicians engaged in the installation, maintenance, cable repair and routine maintenance of DS0 and hi capacity circuits(T1 through OC Level). Additionally, perform quality and safety reviews, assist in employee development and interact with interact and external customers.Assure the highest levels of customer service and achieve safety, reliability and productivity goals within departmental budgets. Execute labor strategies involving employees covered under a collective bargaining agreement. Respond to emergent facility conditions and coordinate emergency response to restore system configuration and/or customers."	  
+	 },
+	 { 
+	    "employer" : " ATT ",
+		"title" : " Manager Network Support",
+		"location": "Chicago, Il",
+		"dates" : "Apr 2010 - Feb 2011",
+		"description" : "Worked with internal and external customers through various mediums to resolve high profile network outages. Used network monitoring tools to monitor network performance and capacity daily. Developed a non-routine process to resolve hardware/software compatibility issues and guided users in required operational changes. Utilized network operation and maintenance concepts to troubleshoot network problems. Provided guidance to field technicians during the installation, configuration and troubleshooting of LAN and WAN components. Assisted with the redesign of network infrastructure in response to changes in network requirements and network technologies."
+		  
+	 },
+	 { 
+	    "employer" : " United States Air Force ",
+		"title" : " Communications/Navigations Systems Specialist",
+		"location": "Fort Dix, New Jersey",
+		"dates" : "Dec 1989 - May 1997",
+		"description" : "Troubleshoot and repaired communication receivers, transmitters, digital and voice satellite communications, multiplexors, electronic switching and radar systems. Analysed testing and troubleshooting data to determine required repair. Diagnosed malfunctions using schematics, wiring diagrams and test equipment. Removed and replaced faulty system wiring, electrical connectors, antennas, transmission lines, and multi conductor cables. Analysed and resolved computer system hardware and software problems. "
+	}
+	]
+}
+function displayWork() {
+for (job in work.jobs) {
+  $("#workExperience").append(HTMLworkStart);
+  
+  var formattedworkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedworkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedworkEmployer + formattedworkTitle;
+  
+  $(".work-entry:last").append(formattedEmployerTitle);
+  
+  var formattedworkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+  $(".work-entry:last").append(formattedworkDates);
+  
+  var formattedworkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+  $(".work-entry:last").append(formattedworkLocation);
+  
+  var formattedworkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  $(".work-entry:last").append(formattedworkDescription);
+ }
+}
+displayWork();
+
+var projects = {
+  "projects": [
+    {
+	  "title": "Orange Udacity Mug",
+	  "dates": "2014",
+	  "description": "You will replicate a design mock-up",
+	  "images" : ["\images/udacitymug.png"]
+	},
+	{
+      "title": "Interactive Resume",
+      "dates": "2014",
+      "description": "Design an Interactive Resume",
+      "images" : ["\images/Resume1.png"]
+    }	   
+	 ]
+	};
+projects.display = function () {
+for (project in projects.projects) {
+  $("#projects").append(HTMLprojectStart);
+  
+  var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+  $(".project-entry:last").append(formattedTitle);
+  
+  var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+  $(".project-entry:last").append(formattedDates);
+  
+  var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+  $(".project-entry:last").append(formattedDescription); 
+  
+  if (projects.projects[project].images.length > 0) {
+    for (image in projects.projects[project].images) {
+	  var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+	  $(".project-entry:last").append(formattedImage);  
+     }
+   }
+ }
+ };
+ projects.display();
+
+$(document).click(function(loc){
+  var x = loc.pageX;
+  var y = loc.pageY;
+  
+  logClicks(x,y);
+ });
+ $('#main').append(internationalizeButton);
+ 
+ $("#mapDiv").append(googleMap);
